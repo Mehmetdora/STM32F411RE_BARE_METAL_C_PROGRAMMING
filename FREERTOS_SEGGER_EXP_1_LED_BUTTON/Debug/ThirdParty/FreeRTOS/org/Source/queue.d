60545d4b6b3e6bd48b95fbcb19917d61d1242913
Mehmetdora <mehmetdora333@gmail.com>
2026-04-01 00:37:26 +0300

    FreeRTOS ile basit bir LED ve buton task çalıştırma projesi örneği , vTaskDelay() kullanılarak led blink süreleri ayarlandı.

---
+++ FREERTOS_SEGGER_EXP_1_LED_BUTTON/Debug/ThirdParty/FreeRTOS/org/Source/queue.d
@@ -0,0 +1,34 @@
+ThirdParty/FreeRTOS/org/Source/queue.o: \
+ ../ThirdParty/FreeRTOS/org/Source/queue.c \
+ /Users/mehmet_dora/STM32CubeIDE/workspace_2.0.0/FREERTOS_SEGGER_EXP_1_LED_BUTTON/ThirdParty/FreeRTOS/org/Source/include/FreeRTOS.h \
+ /Users/mehmet_dora/STM32CubeIDE/workspace_2.0.0/FREERTOS_SEGGER_EXP_1_LED_BUTTON/ThirdParty/FreeRTOS/org/Source/FreeRTOSConfig.h \
+ ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ /Users/mehmet_dora/STM32CubeIDE/workspace_2.0.0/FREERTOS_SEGGER_EXP_1_LED_BUTTON/ThirdParty/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h \
+ /Users/mehmet_dora/STM32CubeIDE/workspace_2.0.0/FREERTOS_SEGGER_EXP_1_LED_BUTTON/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW.h \
+ /Users/mehmet_dora/STM32CubeIDE/workspace_2.0.0/FREERTOS_SEGGER_EXP_1_LED_BUTTON/ThirdParty/SEGGER/SEGGER/SEGGER.h \
+ /Users/mehmet_dora/STM32CubeIDE/workspace_2.0.0/FREERTOS_SEGGER_EXP_1_LED_BUTTON/ThirdParty/SEGGER/Config/Global.h \
+ /Users/mehmet_dora/STM32CubeIDE/workspace_2.0.0/FREERTOS_SEGGER_EXP_1_LED_BUTTON/ThirdParty/FreeRTOS/org/Source/include/projdefs.h \
+ /Users/mehmet_dora/STM32CubeIDE/workspace_2.0.0/FREERTOS_SEGGER_EXP_1_LED_BUTTON/ThirdParty/FreeRTOS/org/Source/include/portable.h \
+ /Users/mehmet_dora/STM32CubeIDE/workspace_2.0.0/FREERTOS_SEGGER_EXP_1_LED_BUTTON/ThirdParty/FreeRTOS/org/Source/include/deprecated_definitions.h \
+ /Users/mehmet_dora/STM32CubeIDE/workspace_2.0.0/FREERTOS_SEGGER_EXP_1_LED_BUTTON/ThirdParty/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ /Users/mehmet_dora/STM32CubeIDE/workspace_2.0.0/FREERTOS_SEGGER_EXP_1_LED_BUTTON/ThirdParty/FreeRTOS/org/Source/include/mpu_wrappers.h \
+ /Users/mehmet_dora/STM32CubeIDE/workspace_2.0.0/FREERTOS_SEGGER_EXP_1_LED_BUTTON/ThirdParty/FreeRTOS/org/Source/include/task.h \
+ /Users/mehmet_dora/STM32CubeIDE/workspace_2.0.0/FREERTOS_SEGGER_EXP_1_LED_BUTTON/ThirdParty/FreeRTOS/org/Source/include/list.h \
+ /Users/mehmet_dora/STM32CubeIDE/workspace_2.0.0/FREERTOS_SEGGER_EXP_1_LED_BUTTON/ThirdParty/FreeRTOS/org/Source/include/queue.h \
+ /Users/mehmet_dora/STM32CubeIDE/workspace_2.0.0/FREERTOS_SEGGER_EXP_1_LED_BUTTON/ThirdParty/FreeRTOS/org/Source/include/task.h
+/Users/mehmet_dora/STM32CubeIDE/workspace_2.0.0/FREERTOS_SEGGER_EXP_1_LED_BUTTON/ThirdParty/FreeRTOS/org/Source/include/FreeRTOS.h:
+/Users/mehmet_dora/STM32CubeIDE/workspace_2.0.0/FREERTOS_SEGGER_EXP_1_LED_BUTTON/ThirdParty/FreeRTOS/org/Source/FreeRTOSConfig.h:
+../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+/Users/mehmet_dora/STM32CubeIDE/workspace_2.0.0/FREERTOS_SEGGER_EXP_1_LED_BUTTON/ThirdParty/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h:
+/Users/mehmet_dora/STM32CubeIDE/workspace_2.0.0/FREERTOS_SEGGER_EXP_1_LED_BUTTON/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW.h:
+/Users/mehmet_dora/STM32CubeIDE/workspace_2.0.0/FREERTOS_SEGGER_EXP_1_LED_BUTTON/ThirdParty/SEGGER/SEGGER/SEGGER.h:
+/Users/mehmet_dora/STM32CubeIDE/workspace_2.0.0/FREERTOS_SEGGER_EXP_1_LED_BUTTON/ThirdParty/SEGGER/Config/Global.h:
+/Users/mehmet_dora/STM32CubeIDE/workspace_2.0.0/FREERTOS_SEGGER_EXP_1_LED_BUTTON/ThirdParty/FreeRTOS/org/Source/include/projdefs.h:
+/Users/mehmet_dora/STM32CubeIDE/workspace_2.0.0/FREERTOS_SEGGER_EXP_1_LED_BUTTON/ThirdParty/FreeRTOS/org/Source/include/portable.h:
+/Users/mehmet_dora/STM32CubeIDE/workspace_2.0.0/FREERTOS_SEGGER_EXP_1_LED_BUTTON/ThirdParty/FreeRTOS/org/Source/include/deprecated_definitions.h:
+/Users/mehmet_dora/STM32CubeIDE/workspace_2.0.0/FREERTOS_SEGGER_EXP_1_LED_BUTTON/ThirdParty/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h:
+/Users/mehmet_dora/STM32CubeIDE/workspace_2.0.0/FREERTOS_SEGGER_EXP_1_LED_BUTTON/ThirdParty/FreeRTOS/org/Source/include/mpu_wrappers.h:
+/Users/mehmet_dora/STM32CubeIDE/workspace_2.0.0/FREERTOS_SEGGER_EXP_1_LED_BUTTON/ThirdParty/FreeRTOS/org/Source/include/task.h:
+/Users/mehmet_dora/STM32CubeIDE/workspace_2.0.0/FREERTOS_SEGGER_EXP_1_LED_BUTTON/ThirdParty/FreeRTOS/org/Source/include/list.h:
+/Users/mehmet_dora/STM32CubeIDE/workspace_2.0.0/FREERTOS_SEGGER_EXP_1_LED_BUTTON/ThirdParty/FreeRTOS/org/Source/include/queue.h:
+/Users/mehmet_dora/STM32CubeIDE/workspace_2.0.0/FREERTOS_SEGGER_EXP_1_LED_BUTTON/ThirdParty/FreeRTOS/org/Source/include/task.h:
